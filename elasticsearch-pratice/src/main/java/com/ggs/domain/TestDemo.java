bbb60e1c21287185703d3c2badc6cfb11fe62501
lianghaohui <lianghaohui@youfantech.cn>
2022-07-08 19:04:05 +0800

    elasticsearch添加https

--- elasticsearch-pratice/src/main/java/com/ggs/domain/TestDemo.java
+++ elasticsearch-pratice/src/main/java/com/ggs/domain/TestDemo.java
@@ -8,6 +8,8 @@ import co.elastic.clients.elasticsearch.core.IndexRequest;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.elasticsearch.core.search.TotalHits;
+import co.elastic.clients.elasticsearch.core.search.TotalHitsRelation;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
@@ -16,10 +18,26 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 
+import javax.net.ssl.SSLContext;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.util.List;
 import java.util.function.Function;
 
 /**
@@ -33,14 +51,28 @@ public class TestDemo {
 
     private static ElasticsearchAsyncClient asyncClient = null;
 
-    private static synchronized void makeConnection() {
+    private static synchronized void makeConnection() throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("starbug", "123456"));
 
-        RestClientBuilder builder = RestClient.builder(new HttpHost("192.168.131.134", 9200))
+        Path caCertificatePath = Paths.get("http_ca.crt");
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        Certificate trustedCa;
+        try (InputStream is = Files.newInputStream(caCertificatePath)) {
+            trustedCa = factory.generateCertificate(is);
+        }
+
+        KeyStore trustStore = KeyStore.getInstance("pkcs12");
+        trustStore.load(null, null);
+        trustStore.setCertificateEntry("ca", trustedCa);
+        SSLContextBuilder sslContextBuilder = SSLContexts.custom()
+                .loadTrustMaterial(trustStore, null);
+        final SSLContext sslContext = sslContextBuilder.build();
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost("192.168.131.134", 9200, "https"))
                 .setHttpClientConfigCallback((httpClientBuilder) ->
-                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+                        httpClientBuilder.setSSLContext(sslContext).setDefaultCredentialsProvider(credentialsProvider)
                 );
 
         RestClient restClient = builder.build();
@@ -52,7 +84,8 @@ public class TestDemo {
         asyncClient = new ElasticsearchAsyncClient(transport);
     }
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, CertificateException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         makeConnection();
 
         Product product = new Product().setId("1").setName("lucy").setPrice(10);
@@ -90,8 +123,7 @@ public class TestDemo {
         }
 
         // 匹配查询
-        String searchText = "bag";
-        SearchResponse<Product> search1 = client.search(s -> s.index("products").query(q -> q.match(t -> t.field("name").query(searchText))), Product.class);
+        String searchText = "Bag";
         SearchResponse<Product> response1 = client.search(s -> s
                         .index("products")
                         .query(q -> q
@@ -103,6 +135,20 @@ public class TestDemo {
                 Product.class
         );
 
+        TotalHits total1 = response1.hits().total();
+        boolean isExactResult = total1.relation() == TotalHitsRelation.Eq;
+
+        if (isExactResult) {
+            System.out.println("There are " + total1.value() + " results");
+        } else {
+            System.out.println("There are more than " + total1.value() + "results");
+        }
+
+        List<Hit<Product>> hits1 = response1.hits().hits();
+        for (Hit<Product> hit : hits1) {
+            Product pd2 = hit.source();
+            System.out.println("Found product " + pd2.getId() + ", score " + hit.score());
+        }
 
     }
 
